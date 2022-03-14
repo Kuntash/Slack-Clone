bf479f8a1461ec97e823e6d192b20ac58a001651
Kuntash <kuntashtashi11@gmail.com>
2022-03-15 01:36:19 +0530

    React Router Setup, file structure cleaning etc

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBYnXXNyLruxiKLS2FUsSzOG3R68DUGCag",
+  authDomain: "slack-clone-kt.firebaseapp.com",
+  projectId: "slack-clone-kt",
+  storageBucket: "slack-clone-kt.appspot.com",
+  messagingSenderId: "826784211953",
+  appId: "1:826784211953:web:42e6f18e48fe8c7ec89a50"
+};
